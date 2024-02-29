3499be9be819399cff1c090327868a6b8457e787
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-03-01 01:47:02 +0200

    first commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,28 @@
+<div class="main-component">
+    <div class="banner">
+      <h1>Welcome to our Website</h1>
+    </div>
+    <div class="video-container">
+      <iframe title="Featured game" width="560" height="315" src="https://www.youtube.com/embed/qjIxI8I-M1k?si=SKfW6nRkUNuGKWGM" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Morning</th>
+            <th>Noon</th>
+            <th>Evening</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Monday</td>
+            <td>Content</td>
+            <td>Content</td>
+            <td>Content</td>
+          </tr>
+          </tbody>
+      </table>
+    </div>
+  </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Footer from "../components/Footer.svelte";
+import Header from "../components/Header.svelte";
+    import Main from "../components/Main.svelte";
+
+</script>
+<main class="flex flex-col">
+    <Header />
+    <Main />
+</main>
